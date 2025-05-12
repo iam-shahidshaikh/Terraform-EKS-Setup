383cf17ac4c6efc4b7bc32c1855e95b9c58481ba
SHAHID SHAIKH <102761235+iam-shahidshaikh@users.noreply.github.com>
2025-05-13 01:47:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -172,6 +172,6 @@ resource "aws_iam_role_policy_attachment" "devopsshahid_node_group_cni_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "devopsshahid_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshachid_node_group_role.name
+  role       = aws_iam_role.devopsshahid_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
